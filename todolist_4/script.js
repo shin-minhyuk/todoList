a0dd1ecef7a2eb44ce112fab99424b3023031332
shin-minhyuk <afs_style@naver.com>
2024-07-23 00:53:19 +0900

    [투두4] 생성함수, 추가함수, 삭제함수, 수정함수 추가

--- todolist_4/script.js
+++ todolist_4/script.js
@@ -14,14 +14,7 @@ let todoArr = [];
 
 fab.addEventListener("click", () => { popup.style.display = "block" })
 closeBtn.addEventListener("click", () => { popup.style.display = "none" })
-submitBtn.addEventListener("click", () => {
-  if (inpText.value && inpTime.value) {
-    createTodoItem()
-    // popup.style.display = "none"
-  } else {
-    alert ("내용을 작성해주세요.")
-  }
-})
+submitBtn.addEventListener("click", addTodo)
 
 document.addEventListener("click", (event) => {
   const target = event.target;
@@ -38,23 +31,90 @@ document.addEventListener("click", (event) => {
 
   if (target.classList.contains("delete")) {
     const todoItem = target.closest(".todo-item");
-    console.log(todoItem)
+
+    // todoItem => <li class="todo-item" data-index="0">
     deleteItem(todoItem)
+  }
+
+  if (target.classList.contains("change")) {
+    const todoItem = target.closest(".todo-item");
+    todoItem.querySelector(".menu-box").style.display = "none"
+
+    // todoItem => <li class="todo-item" data-index="0">
+    changeItem(todoItem)
   }
 })
 
-function deleteItem() {
+function changeItem(todoItem) {
+  // 1. popup display:block; 
+  // 2. 객체값 기반 todoItem.time, todoItem.text 출력
+  // 3. 할일 추가 버튼을 눌렀을 때 다시 그 인덱스 번호로 배열에 집어넣어야함.
 
+
+  // const inpTime = document.querySelector("#time")
+  // const inpText = document.querySelector("#text")
+  const index = todoItem.dataset.index
+  const todo = todoArr[index]
+  console.log(todo)
+
+
+  if (todo) {
+    // console.log(todo.time)
+    // console.log(inpTime.value)
+    inpTime.value = todo.time
+    inpText.value = todo.text
+    popup.style.display = "block";
+
+    submitBtn.removeEventListener("click", addTodo);
+    submitBtn.addEventListener("click", function updateTodo() {
+      if (inpTime.value && inpText.value) {
+        todo.time = inpTime.value
+        todo.text = inpText.value
+        displayTodos(todoArr);
+        popup.style.display = "none";
+  
+        submitBtn.removeEventListener("click", updateTodo)
+        submitBtn.addEventListener("click", addTodo)
+      } else {
+        alert ("내용을 작성하세요.")
+      }
+    })
+  }
+
+  // todoArr.forEach((todo, todoIndex) => {
+  //   if (todoIndex === Number(index)) {
+  //     console.log(inpTime.value)
+  //     submitBtn.addEventListener("click", () => {
+  //       todoArr = todoArr.splice(todoIndex, 1)
+  //       displayTodos(todoArr)
+  //     })
+  //   }
+
+  //   if (inpText.value && inpTime.value) {
+  //     createTodoItem()
+  //     // popup.style.display = "none"
+  //   } else {
+  //     alert ("내용을 작성해주세요.")
+  //   }
+  // })
 }
 
-// function deleteItem(clickedId) {
-//   todoArr = todoArr.filter((todo) => {
-//     if (todo.id !== clickedId) {
-//       return todo
-//     }
-//   })
-//   displayTodos(todoArr)
-// }
+function deleteItem(todoItem) {
+  const index = todoItem.dataset.index
+  console.log(index) // 정상출력
+
+  // 필터를 돌려서 만약 todo의 인덱스값과
+  // 선택된 아이템의 인덱스 값이 같지 않은상황에는 전부 리턴
+  todoArr = todoArr.filter((todo, todoIndex) => {
+    // console.log(todoIndex)
+
+    
+    if (todoIndex !== Number(index)) { 
+      return todo
+    }
+  })
+  displayTodos(todoArr)
+} 
 
 function createTodoItem() {
   const todoItem = {
@@ -65,23 +125,16 @@ function createTodoItem() {
 
   todoArr.push(todoItem)
   displayTodos(todoArr)
-  console.log(todoArr)
+  setLocalstorage()
 }
 
-// function deleteBtn(clickedId) {
-//   const id = todos.dataset.id
-//   if (todoArr.forEach((todo) => {
-    
-//   }))
-// }
-
-function displayTodos(todos) {
+function displayTodos(todos) { 
   todoList.innerHTML = ""
   
-  todos.forEach((todo) => {
+  todos.forEach((todo, index) => { // 매개변수 (1.배열의 값, 2.인덱스, 3.전체배열)
     const todoLi = document.createElement("li")
     todoLi.classList.add("todo-item")
-    todoLi.dataset.id = id
+    todoLi.dataset.index = index
 
     todoLi.innerHTML = `
     <button class="menu-btn">
@@ -104,3 +157,27 @@ function displayTodos(todos) {
     todoList.append(todoLi)
   })
 }
+
+function addTodo() {
+  if (inpText.value && inpTime.value) {
+    createTodoItem() 
+    popup.style.display = "none"
+  } else {
+    alert ("내용을 작성해주세요.")
+  }
+}
+
+function setLocalstorage() {
+  const stringTodoArr = JSON.stringify(todoArr)
+  localStorage.setItem("todo", stringTodoArr)
+}
+
+function getLocalstorage() {
+  const getLocalTodoArr = localStorage.getItem("todo")
+  if (getLocalTodoArr !== null) {
+    todoArr = JSON.parse(getLocalTodoArr)
+    displayTodos(todoArr)
+  }
+}
+
+getLocalstorage()
